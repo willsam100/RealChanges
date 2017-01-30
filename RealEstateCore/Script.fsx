a233390616b22e6cde756923850e24388319a083
Sam Williams <willsam100@gmail.com>
2017-01-31 07:04:10 +1300

    Property script

--- RealEstateCore/Script.fsx
+++ RealEstateCore/Script.fsx
@@ -1,72 +1,126 @@
-﻿#r "../packages/FSharp.Data.2.3.2/lib/net40/FSharp.Data.dll"
+﻿//#r "../packages/FSharp.Data.2.3.2/lib/net40/FSharp.Data.dll"
+//#load "ListingDownloader.fs"
+#r "/Users/willsam100/Oooby/mobile-app/packages/FsCheck.2.6.2/lib/net45/FsCheck.dll"
+//open ListingDownloader
+//open FSharp.Data
+open FsCheck
+open System
+open FsCheck
 
-open FSharp.Data
+type AppEvent = Work | Drive | Rest | FinishWork
 
-let run () = 
-    let data = System.IO.File.ReadAllText("/Users/willsam100/Desktop/list.html")
-    let body = HtmlDocument.Parse(data) //|> Async.RunSynchronously
+let minVal = DateTime(0L) |> fun x -> x.Ticks
+let maxVal = DateTime(0L) |> fun x -> x.AddDays 1. |> fun x -> x.Ticks
 
-    //let tableWithPrice = "ListingAttributes"
-    //let tableRows = "tr"
+printfn "MinVal: %d, maxVal: %d" minVal maxVal
 
-    let header = 
-        body.Descendants ["h1"] 
-        |> Seq.choose (fun x -> x.TryGetAttribute("itemprop") |> Option.map (fun a -> a.Value(), x)) 
-        |> Seq.filter (fun (x, y) -> x = "name")
-        |> Seq.map (fun (x,y) -> y.InnerText().Trim())
-        |> Seq.tryHead
-        
-    let askingPrice = 
-        body.Descendants ["h2"] 
-        |> Seq.map (fun (x) -> x.InnerText().Trim())
-        |> Seq.tryHead
+let dayGen xs = 
+  gen { let! i = Gen.choose (0, List.length xs-1) 
+        return List.item i xs }
 
-    let image = 
-        body.Descendants ["div"]
-        |> Seq.choose (fun x -> x.TryGetAttribute("id") |> Option.map (fun a -> a.Value(), x)) 
-        |> Seq.filter (fun (x, y) -> x = "mainImageHolder")
-        |> Seq.map (fun (x,y) -> y.Descendants ["img"])
-        |> Seq.concat
-        |> Seq.choose (fun x -> x.TryGetAttribute "src" |> Option.map (fun y -> y.Value ()))
-        |> Seq.tryHead
-        
-        
-    let views = 
-        body.Descendants ["span"]
-        |> Seq.choose (fun x -> x.TryGetAttribute("class") |> Option.map (fun a -> a.Value(), x)) 
-        |> Seq.filter (fun (x, y) -> x = "stats")
-        |> Seq.map (fun (x,y) -> y.Descendants ["a"])
-        |> Seq.concat
-        |> Seq.choose (fun x -> x.TryGetAttribute "href" |> Option.map (fun a -> x))
-        |> Seq.map (fun x -> x.InnerText())
-        |> Seq.filter (fun x -> x.Contains "Listing Views")
-        |> Seq.tryHead
-        |> Option.map (fun x -> x.Replace("Listing Views", "").Trim())
-        
-    let listingId = 
-        body.Descendants ["h4"]
-        |> Seq.map (fun x -> x.Descendants ["b"])
-        |> Seq.concat
-        |> Seq.map (fun x -> x.InnerText())
-        |> Seq.filter (fun x -> x.Contains "Listing")
-        |> Seq.tryHead
-        |> Option.map (fun x -> x.Replace("Listing", "").Replace("#", "").Trim())
-        
-        
-        
+let pickOne = Seq.unfold (let nextSecond x = x + (10L * 1000000L)
+                          function
+                           | st when (nextSecond st) > maxVal -> None
+                           | st -> Some (st, nextSecond st) ) 
+                        minVal
 
-//let run () = 
-//    let data = System.IO.File.ReadAllText("/Users/willsam100/Desktop/list 4.html")
-//    let body = HtmlDocument.Parse(data) //|> Async.RunSynchronously
 
-//    let links = body.Descendants ["p"]
-//                //|> Seq.choose (fun x -> x.TryGetAttribute "href" |> Option.map(fun y -> y.Value(), x))
-//                |> Seq.map (fun (x) -> x.InnerText())
-//                |> Seq.map (fun x -> x = "This listing was withdrawn by the administrator")
-//                |> Seq.fold (fun x y -> x || y) false
-//    //            |> Seq.filter (fun x -> x.Contains("?id="))
-//    //            |> Seq.map (fun x -> x.Replace("/Browse/Listing.aspx?id=", ""))
+let matrix gen = Gen.sized <| fun s -> Gen.resize (s|>float|>sqrt|>int) gen
 
-//    //links |> Seq.iter (printfn "%A")
-    printfn "header %A" listingId
-run ()
+let genListEvents biasEvent = 
+    let genNextEvent = function
+        | Work ->       [Drive; Rest; FinishWork]
+        | Drive ->      [Work; Rest; FinishWork]
+        | Rest ->       [Drive; Work; FinishWork]
+        | FinishWork -> [Drive; Rest; Work]
+
+    let toGenNextEvent e = 
+
+        let computeProbs xs = 
+            xs |> List.contains biasEvent |> function
+            | true -> (8, 1)
+            | false -> (1, 1)
+
+        let nextEvents = e |> genNextEvent 
+        let probs = computeProbs nextEvents
+
+        nextEvents |> List.map (fun x -> (if (x = biasEvent) then (fst <| probs) else (snd probs)), x)
+        |> List.map (fun (x,y) -> (x, Gen.constant y)) 
+        |> Gen.frequency
+
+    let ininitalValue = Gen.constant FinishWork |> Gen.listOfLength 1
+    let rec genListEventsInReverse (events: Gen<AppEvent list>) s =
+
+        let growList () = 
+            gen {
+                let! v = events
+                let lastValue = List.head v
+                let! nextValue = toGenNextEvent lastValue
+                return (nextValue :: v)
+            } 
+
+        let genListReverse gxs = 
+            gen { 
+                let! xs = gxs
+                return xs |> List.rev
+            }
+
+        match (s/4) with 
+        | s when s < 0 -> events |> genListReverse
+        | 0 -> events |> genListReverse
+        | s ->  genListEventsInReverse (growList ()) (s-1)
+
+    Gen.sized <| genListEventsInReverse ininitalValue
+
+
+let genListEventsWithTimes biasEvent maxWork maxRest = 
+    let addMinutes x =
+        let compute max = Gen.choose (0, max) |> Gen.map (fun y -> (x,y))
+        match x with 
+        | Drive | Work -> compute maxWork
+        | Rest | FinishWork -> compute maxRest
+    genListEvents biasEvent >>= (fun xs -> xs |> List.map addMinutes |> Gen.sequence)
+
+type AppEventArb =
+  static member AppEvents() =
+      {new Arbitrary<List<AppEvent*int>>() with
+          override x.Generator = genListEventsWithTimes Drive (11 * 60) 30 }
+
+
+Arb.register<AppEventArb>()
+
+
+let validEvents (xs: (AppEvent*int) list) = 
+    xs |> List.pairwise |> List.forall (fun (x,y) -> (fst x) <> (fst y))
+
+let biasIsCorrect (xs: (AppEvent * int) list) = 
+    let countBias e = 
+        xs |> List.fold (fun s x -> if (fst x = e) then s + 1 else s) 0
+
+    let driveCount = countBias Drive 
+    let workCount = countBias Work 
+    let restCount = countBias Rest 
+    let fwCount = countBias FinishWork 
+
+    let givenContainsDriveEvent = List.fold (fun r x -> r || fst x = Drive) false xs
+
+    givenContainsDriveEvent ==> 
+        (driveCount + 1 >= workCount && 
+            driveCount + 1 >= restCount && 
+            driveCount + 2 >= fwCount)
+
+
+let hasSufficientDriving (xs: (AppEvent * int) list) = 
+    let hasEnoughDriving = 
+        xs 
+        |> List.fold (fun s (x,y) -> match x with 
+                                     | Work | Drive -> s + y
+                                     | Rest | FinishWork -> s) 0
+        |> (fun x -> printfn "Total Driving Hours: %d" (x/60); x)
+        |> (fun x -> x > (11 * 60))
+
+    hasEnoughDriving ==> biasIsCorrect xs
+
+printfn "Count: %d" <| Seq.length pickOne
+Seq.head pickOne |> DateTime |> printfn "Head: %A"
+Seq.last pickOne |> DateTime |> printfn "Last: %A"
